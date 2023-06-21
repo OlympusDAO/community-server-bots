df0e7a2abe3745f19f0b950e87110cd4934f2fe8
0xrusowsky <0xrusowsky@proton.me>
2023-06-21 22:21:12 +0200

    feat: setup bot + update nickname

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,27 @@
+import { Client, GatewayIntentBits, GuildMember } from "discord.js";
 import { createClient, Operations, Queries } from "@olympusdao/treasury-subgraph-client";
 import { getMetricsLatestCompleteData, getTokensLatestCompleteData, getSuppliesLatestCompleteData } from "subgraph/subgraph";
 import { getLiquidBackingPerOhmBacked, getTreasuryAssetValue, getOhmCirculatingSupply } from "subgraph/helpers";
+
+let guildMeCache: GuildMember[] = [];
+
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user!.tag}!`);
+    bot.user?.setActivity(`😀`);
+
+    bot.guilds.cache.each(guild => guild.members.me && guildMeCache.push(guild.members.me));
+    getProtocolMetrics();
+});
+
+// New server join event that causes the guild cache to refresh
+bot.on('guildCreate', guild => {
+    bot.guilds.cache.each(guild => guild.members.me && guildMeCache.push(guild.members.me));
+    console.log(`New server has added the bot! Name: ${guild.name}`);
+});
+
+bot.login(process.env.DISCORD_BOT_TOKEN);
 
 const client = createClient({
     baseURL: 'http://127.0.0.1:9991/',
@@ -26,6 +47,9 @@ async function getProtocolMetrics() {
         const ohmCirculatingSupply = getOhmCirculatingSupply(latestSupplyData, currentIndex)[0]
         const ohmMarketCap = ohmPrice * ohmCirculatingSupply;
 
-        console.log(ohmMarketCap);
+        guildMeCache.forEach(guild => guild.setNickname(
+            `${(ohmMarketCap / 1e6).toLocaleString('en-us', { maximumFractionDigits: 2, minimumFractionDigits: 2 })}M`
+        ));
+        bot.user?.setActivity(`OHM MarketCap`);
     }
 }
